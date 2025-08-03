08b2e4627df2431349f83f13c9ca8ce5825777f4
opsonusdh <sumanasensen262@gmail.com>
2025-08-03 10:19:57 +0530

    Update Byomkesh.py

--- Byomkesh.py
+++ Byomkesh.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
            OOb"                                                                     """+RESET)
         print(RED+"              https://github.com/opsonusdh/Byomkesh"+RESET)
         print('\n\n')
-        print(f'Finding {user}...')
+        print(f'Finding {user} in 300+ sites...')
         check_username(user, num)
     except ValueError:
         print("Using 1 Thread.")
